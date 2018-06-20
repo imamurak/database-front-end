dafbcabcbd04c3fc924511ed1b1fb1fbf2a427a9
Kirk Imamura <kirk.kokoro@gmail.com>
2018-06-20 09:37:32 -0400

    Weekly Update
    Changed image
    Added JSON file
    Tried adding javascript

---
+++ script2.js
@@ -0,0 +1,8 @@
+// Parse data from JSON file
+var sitedata = JSON.parse(int_sites);
+var site.name = sitedata[0].name;
+var site.url = sitedata[0].url;
+var site.descr = sitedata[0].descr;
+var site.tags = sitedata[0].tags;
+var site.date = sitedata[0].date;
+var site.notes = sitedata[0].notes;
